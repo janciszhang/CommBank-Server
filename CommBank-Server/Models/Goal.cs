b3a8bb510c0f822f7823321a2301447850734b18
jzha0336 <jzha0336@student.monash.edu>
2023-09-05 05:08:53 +1000

    For Goals.cs, Add an optional public Icon field of string type

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -27,4 +27,7 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
+    
+    // Add an optional public Icon field of string type
+    public string? Icon { get; set; }
 }
